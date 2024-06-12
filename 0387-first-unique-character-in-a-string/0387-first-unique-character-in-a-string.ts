6e14f54fb82f734681452f20ebd6d42ac6def438
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-12 12:21:11 +0900

    Time: 85 ms (79.18%), Space: 54 MB (65.36%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.ts
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.ts
@@ -1,16 +1,9 @@
 function firstUniqChar(s: string): number {
-    const hashMap = {};
 
     for (let i = 0; i < s.length; i++) {
-        if (hashMap[s[i]]) { hashMap[s[i]] += 1 }
-        else {
-            hashMap[s[i]] = 1
-        }
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
     }
 
+    return -1;
 
-    const uniqueCharacter = Object.entries(hashMap).find((item) => item[1] === 1)?.[0];
-
-
-    return s.split("").findIndex(char => char === uniqueCharacter)
 };
