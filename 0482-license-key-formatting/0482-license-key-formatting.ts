d20aa4b3bf6db69b57a8d504c600f8516cc3a687
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-16 23:08:56 +0900

    Time: 171 ms (7.69%), Space: 59.1 MB (9.89%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.ts
+++ 0482-license-key-formatting/0482-license-key-formatting.ts
@@ -1,8 +1,4 @@
 function licenseKeyFormatting(s: string, k: number): string {
-    // 주어진 s에서 - 제거한 문자배열로 만들기 
-    // 모든 문자 대문자화하기 
-    // 뒤에서부터 원하는 개수 만큼 split하기 
-
     const letters = s.split("").filter(letter => letter !== "-").map(letter => letter.toUpperCase()).join("");
     let result: string[] = [];
     let length = letters.length
