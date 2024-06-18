c858e9d65ca8c04a682497ffdb312f1bdf73c061
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-18 14:51:45 +0900

    Time: 58 ms (65.21%), Space: 51.7 MB (56.91%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -2,6 +2,5 @@ function intersection(nums1: number[], nums2: number[]): number[] {
     const uniqueNums1 = new Set(nums1)
     const uniqueNums2 = new Set(nums2)
 
-    return [...uniqueNums1].filter(item => [...uniqueNums2].includes(item))
-
+    return [...uniqueNums1].filter(number => uniqueNums2.has(number))
 };
