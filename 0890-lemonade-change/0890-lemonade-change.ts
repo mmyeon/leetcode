0668bff28819db226ede17c0c32b7ab73ea5d24b
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-20 11:23:24 +0900

    Time: 66 ms (94.78%), Space: 58.7 MB (90.43%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.ts
+++ 0890-lemonade-change/0890-lemonade-change.ts
@@ -1,26 +1,25 @@
 function lemonadeChange(bills: number[]): boolean {
     // 5달러 지폐, 10 달러 지폐 추적하기 
-    let fiveDollarNumber = 0, tenDollarNumber = 0;
+    let fiveDollarCount = 0, tenDollarCount = 0;
 
     for (let i = 0; i < bills.length; i++) {
         if (bills[i] === 5) {
-            fiveDollarNumber++;
+            fiveDollarCount++;
         } else if (bills[i] === 10) {
-            if (fiveDollarNumber >= 1) {
-                fiveDollarNumber--;
-                tenDollarNumber++;
+            if (fiveDollarCount >= 1) {
+                fiveDollarCount--;
+                tenDollarCount++;
             } else {
-                console.log("10", fiveDollarNumber, tenDollarNumber)
                 return false
             };
         } else if (
             bills[i] === 20
         ) {
-            if (fiveDollarNumber >= 1 && tenDollarNumber >= 1) {
-                fiveDollarNumber--;
-                tenDollarNumber--;
-            } else if (fiveDollarNumber >= 3) {
-                fiveDollarNumber -= 3;
+            if (fiveDollarCount >= 1 && tenDollarCount >= 1) {
+                fiveDollarCount--;
+                tenDollarCount--;
+            } else if (fiveDollarCount >= 3) {
+                fiveDollarCount -= 3;
             } else {
                 return false;
             };
