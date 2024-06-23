861712bbf934dd8015f01bcea0bfdbf0ffc31ba7
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-23 21:35:44 +0900

    Time: 82 ms (90.62%), Space: 71.2 MB (24.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -1,25 +1,13 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-    // 맵 만들기 
-    let map = {};
+    let map: Map<number, number> = new Map();
     for (let i = 0; i < nums.length; i++) {
-        // 순회하면서 인덱스 저장하기 
-        if (map[nums[i]] === undefined) {
-            console.log("처음 키가 추가됨 ")
-            map[nums[i]] = i;
-        } else {
-            // 맵에 해당 값 있는지 체크 
-            // 있으면 현재 인덱스와 저장된 인덱스가 k 이하인지 체크 
-            // 그러면 true 리턴
-            if ((i - map[nums[i]]) <= k) {
+        if (map.has(nums[i])) {
+            if ((i - map.get(nums[i])) <= k)
                 return true
-            }
-            else {
-                // 현재 값이 있는데 k인덱스차이보다 크면 현재 index로 업데이트하기 
-                map[nums[i]] = i
-            }
-        }
-    }
 
-    // 반복문 끝나면 false 리턴하기 
+        }
+
+        map.set(nums[i], i)
+    }
     return false;
 };
