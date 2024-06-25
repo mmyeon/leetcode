5b89272bebdadfd997cbda3ec80fc2a9c0b75082
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-25 16:01:49 +0900

    Time: 61 ms (31.36%), Space: 51.5 MB (24.94%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.ts
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.ts
@@ -11,26 +11,14 @@
  */
 
 function middleNode(head: ListNode | null): ListNode | null {
-    // 노드의 전체 사이즈를 알기 위해서 Map 자료구조에 개별 노드 저장하기 
-    let current = head;
-    let map = new Map();
-    let index = 0;
+    // 2개의 포인터 사용
+    let slow = head;
+    let fast = head;
 
-    while (current.next !== null) {
-        // 현재 노드와 인덱스 저장 
-        map.set(index, current)
-        // 다음 노드로 이동 
-        current = current.next;
-        index++;
+    while (fast && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
 
-    // 노드가 1개 일 때는, head 노드 리턴하도록 처리
-    if (map.size === 0) return head;
-    if (map.size === 1) return head.next;
-
-    // 노드 사이즈를 알아낸 뒤, 2로 나눠서 중앙 index가 됨 
-    const middleNodeIndex = map.size % 2 === 0 ? Math.floor(map.size / 2) : Math.floor(map.size / 2) + 1
-
-    // 해당 index에 해당하는 노드 리턴하기 
-    return map.get(middleNodeIndex)
+    return slow;
 };
