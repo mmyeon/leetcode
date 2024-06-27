249c852a49090fa06d5da2f2654af8173107b7e1
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-27 11:27:04 +0900

    Time: 116 ms (87.24%), Space: 73.5 MB (66.25%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.ts
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.ts
@@ -10,14 +10,42 @@
  * }
  */
 
-function isPalindrome(head: ListNode | null): boolean {
-    let leftToRightArr = [];
-    let current = head; 
+const reverseList = (head: ListNode | null): ListNode | null => {
+    let prev = null;
+    let current = head;
 
     while (current) {
-        leftToRightArr.push(current.val)
-        current = current.next;
+        let next = current.next;
+        current.next = prev;
+        prev = current
+        current = next;
     }
 
-    return leftToRightArr.join() === leftToRightArr.slice().reverse().join()
+    return prev;
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+    if (!head || !head.next) return true;
+
+    // 중간 노드 찾기 
+    let slowP = head;
+    let fastP = head;
+
+    while (fastP && fastP.next) {
+        slowP = slowP.next;
+        fastP = fastP.next.next;
+    }
+
+    // 중간 노드 이후 노드를 뒤집기 
+    let firstHalfStart = head;
+    let secondHalfStart = reverseList(slowP);
+
+    // 동일값인지 비교하기 
+    while (secondHalfStart) {
+        if (firstHalfStart.val !== secondHalfStart.val) return false;
+        firstHalfStart = firstHalfStart.next;
+        secondHalfStart = secondHalfStart.next;
+    }
+
+    return true;
 };
