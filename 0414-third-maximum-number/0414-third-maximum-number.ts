8dd8a362254aef6b7f82115987f6aa52d19fddb9
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-06-29 23:12:27 +0900

    Time: 60 ms (66.82%), Space: 52.1 MB (47%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.ts
@@ -0,0 +1,5 @@
+function thirdMax(nums: number[]): number {
+    const sortedNum = Array.from(new Set(nums.sort((a, b) => b - a)))
+
+    return sortedNum.length > 2 ? sortedNum[2] : sortedNum[0];
+};
