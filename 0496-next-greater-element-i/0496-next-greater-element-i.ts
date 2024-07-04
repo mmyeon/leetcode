62ce947e681553501654612652f7cb123a00baeb
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-07-04 20:02:15 +0900

    Time: 53 ms (97.59%), Space: 53 MB (42.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.ts
+++ 0496-next-greater-element-i/0496-next-greater-element-i.ts
@@ -1,14 +1,12 @@
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-    let stack = [];
-    let hashTable = new Map();
+    let stack: number[] = [];
+    let hashTable: Map<number, number> = new Map();
 
     for (const num of nums2) {
-        // num이 stack 숫자보다 큰 경우, hashTable에 저장하기 
         while (stack.length > 0 && stack[stack.length - 1] < num) {
             hashTable.set(stack.pop(), num);
         }
 
-        // nums2 숫자 삽입 
         stack.push(num)
     }
 
