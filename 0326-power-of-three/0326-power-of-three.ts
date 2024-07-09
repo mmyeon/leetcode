184633d457663f4d6c9ef6aa0d11c7b62766be4e
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-07-09 16:00:00 +0900

    Time: 146 ms (87%), Space: 59.8 MB (24.66%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.ts
@@ -0,0 +1,23 @@
+function isPowerOfThree(n: number): boolean {
+    let isPowerOfThree = false;
+
+    if (n <= 0) return isPowerOfThree;
+
+    const checkPowerOfThree = (n: number) => {
+        if (n === 1) { isPowerOfThree = true } else {
+
+            if (n % 3 !== 0) {
+                isPowerOfThree = false;
+                return;
+            } else {
+                return checkPowerOfThree(n / 3)
+            }
+
+        }
+    }
+
+    checkPowerOfThree(n)
+
+
+    return isPowerOfThree;
+};
