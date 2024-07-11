74219c651628aff6b6246708f6f6f4f828b19899
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-07-11 14:18:11 +0900

    Time: 60 ms (83.43%), Space: 52.5 MB (80.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.ts
+++ 0205-isomorphic-strings/0205-isomorphic-strings.ts
@@ -1,20 +1,13 @@
 
 function isIsomorphic(s: string, t: string): boolean {
     let mapS = new Map();
-    let mappedT = new Set();
+    let mapT = new Map();
 
     for (let i = 0; i < s.length; i++) {
-        let charS = s[i];
-        let charT = t[i];
+        if (mapS.get(s[i]) !== mapT.get(t[i])) return false;
 
-        if (mapS.has(charS)) {
-            if (mapS.get(charS) !== charT) return false;
-        } else {
-            if (mappedT.has(charT)) return false;
-
-            mapS.set(charS, charT);
-            mappedT.add(charT);
-        }
+        mapS.set(s[i], i)
+        mapT.set(t[i], i)
     }
 
     return true;
