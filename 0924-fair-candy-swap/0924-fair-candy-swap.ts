e48aab7416319961081e9eeba8a4bb32fe77b2ee
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-07-21 21:10:27 +0900

    Time: 76 ms (90.32%), Space: 59 MB (45.16%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.ts
+++ 0924-fair-candy-swap/0924-fair-candy-swap.ts
@@ -1,11 +1,11 @@
 function fairCandySwap(aliceSizes: number[], bobSizes: number[]): number[] {
     const aliceCandySum = aliceSizes.reduce((acc, cur) => acc + cur, 0);
     const bobCandySum = bobSizes.reduce((acc, cur) => acc + cur, 0);
-    const delta = (bobCandySum - aliceCandySum) / 2
+    const diff = (bobCandySum - aliceCandySum) / 2
     const bobSizesSet = new Set(bobSizes)
 
     for (const aliceCandyNum of aliceSizes) {
-        const bobCandyNum = aliceCandyNum + delta;
+        const bobCandyNum = aliceCandyNum + diff;
 
         if (bobSizesSet.has(bobCandyNum))
             return [aliceCandyNum, bobCandyNum]
