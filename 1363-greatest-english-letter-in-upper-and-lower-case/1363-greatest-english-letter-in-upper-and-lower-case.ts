e557cc052007d21d6cee956662dc18a0a91bbf4b
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-07-22 13:30:23 +0900

    Time: 72 ms (54.55%), Space: 52.5 MB (77.27%) - LeetHub

--- 1363-greatest-english-letter-in-upper-and-lower-case/1363-greatest-english-letter-in-upper-and-lower-case.ts
+++ 1363-greatest-english-letter-in-upper-and-lower-case/1363-greatest-english-letter-in-upper-and-lower-case.ts
@@ -1,13 +1,21 @@
 function greatestLetter(s: string): string {
     let result = "";
+    let upperCaseSet = new Set()
+    let lowerCaseSet = new Set()
 
-    const charCodeAt = s.split("").map(letter => letter.charCodeAt(0))
-    const upperCaseLetters = new Set(charCodeAt.filter(letterCode => letterCode < 97).sort())
-    const lowerCaseLetters = new Set(charCodeAt.filter(letterCode => letterCode >= 97))
+    for (const letter of s) {
+        const letterCode = letter.charCodeAt(0)
+        if (65 <= letterCode && letterCode <= 90) {
+            upperCaseSet.add(letterCode)
+        } else if (
+            97 <= letterCode && letterCode <= 122
+        ) { lowerCaseSet.add(letterCode) }
+    }
 
-    for (const upperCaseLetter of upperCaseLetters) {
-        if (lowerCaseLetters.has(upperCaseLetter + 32)) {
-            result = String.fromCharCode(upperCaseLetter)
+    for (let i = 90; i >= 65; i--) {
+        if (upperCaseSet.has(i) && lowerCaseSet.has(i + 32)) {
+            result = String.fromCharCode(i)
+            break
         }
     }
 
