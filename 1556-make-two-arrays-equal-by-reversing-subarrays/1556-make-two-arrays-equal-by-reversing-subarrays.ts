02cebc524fea53ab11ed1af468e82acb6f9b49e7
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-07-30 13:39:07 +0900

    Time: 59 ms (81.82%), Space: 51.8 MB (72.73%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.ts
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.ts
@@ -1,10 +1,6 @@
 function canBeEqual(target: number[], arr: number[]): boolean {
-  const sortedTarget = target.sort();
-  const sortedArr = arr.sort();
+  const sortedTarget = target.sort((a,b) => a-b);
+  const sortedArr = arr.sort((a,b) => a-b);
 
-  for(let i = 0; i < sortedTarget.length;i++){
-    if(sortedTarget[i] !== sortedArr[i]) return false
-  } 
-
-  return true
+  return sortedTarget.every((target,index) => target === sortedArr[index]) 
 };
