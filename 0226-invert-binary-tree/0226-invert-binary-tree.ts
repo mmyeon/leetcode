9767e46fc05e1c53a06bc221a9647d3df83c5663
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-08-05 10:57:06 +0900

    Time: 53 ms (82.61%), Space: 51.5 MB (81.41%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.ts
+++ 0226-invert-binary-tree/0226-invert-binary-tree.ts
@@ -14,18 +14,16 @@
 
 function invertTree(root: TreeNode | null): TreeNode | null {
     // root가 비어있는 경우 처리
-    if(!root) return root; 
-    // root만 있는 경우 처리
-    if(!root.left && !root.right) return root; 
+    if(!root) return root;
 
     // 자식 노드가 둘 다 있는 경우 교체 
     let temp = root.left
     root.left = root.right
     root.right = temp
 
-    // 하위 트리도 교체 작업 반복하기
-    if(root.left) invertTree(root.left)
-    if(root.right) invertTree(root.right)
+    // 하위 노드도 교체 작업 반복하기
+    invertTree(root.left)
+    invertTree(root.right)
 
     return root
 };
