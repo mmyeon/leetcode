132620a46f950e1a77fd3773672cefe5b62acdad
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-08-08 13:57:52 +0900

    Time: 78 ms (58.11%), Space: 57.1 MB (50.22%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.ts
+++ 0338-counting-bits/0338-counting-bits.ts
@@ -1,17 +1,9 @@
 function countBits(n: number): number[] {
-    let result: number[] = [];
-    for (let i = 0; i <= n; i++) {
-        let count = 0;
-        let value = i
+    let result: number[] = new Array(n+1).fill(0)
 
-        while (value > 0) {
-            // 마지막 비트가 1인지 체크 
-            count += value & 1
-            // 비트 오른쪽으로 이동 
-            value >>= 1
-        }
-        result.push(count);
+    for(let i = 0; i <= n; i++) {
+        result[i] = result[i >> 1] + (i & 1)
     }
-
-    return result
+  
+    return result 
 };
