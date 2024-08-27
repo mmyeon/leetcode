9ffede5827d96cb4ca5c5de339dc659bbdef0cde
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-08-27 17:30:28 +0900

    Time: 59 ms (39.91%), Space: 50.1 MB (78.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -1,16 +1,18 @@
 function rob(nums: number[]): number {
-
     if (nums.length === 1) return nums[0]
     if (nums.length === 2) return Math.max(nums[0], nums[1])
 
-
-    let dp = [];
-    dp[0] = nums[0]
-    dp[1] = Math.max(nums[0], nums[1])
+    // 알아야 하는 값 
+    // prev1 = dp[i-1]
+    // prev2 = dp[i-2]
+    let prev2 = nums[0];
+    let prev1 = Math.max(nums[0], nums[1]);
 
     for (let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+        const current = Math.max(prev1, prev2 + nums[i]);
+        prev2 = prev1
+        prev1 = current
     }
 
-    return dp[dp.length - 1]
+    return prev1
 };
