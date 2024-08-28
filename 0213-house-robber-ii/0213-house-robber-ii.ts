0616ff6d997840f47fa92242453782c9b85ce149
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-08-28 19:01:12 +0900

    Time: 51 ms (84.42%), Space: 51.5 MB (35.68%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.ts
+++ 0213-house-robber-ii/0213-house-robber-ii.ts
@@ -1,28 +1,17 @@
 function rob(nums: number[]): number {
-    if (nums.length === 1) return nums[0]
-    if (nums.length === 2) return Math.max(nums[0], nums[1])
+    if (nums.length === 1) return nums[0];
 
+    const dp = (start, end) => {
+        let prev = 0, curr = 0;
 
-    let firstHouseDpPrev2 = nums[0]
-    let firstHouseDpPrev1 = Math.max(nums[0], nums[1])
-    let lastHouseDpPrev2 = nums[1];
-    let lastHouseDpPrev1 = Math.max(nums[1], nums[2]);
+        for (let i = start; i < end; i++) {
+            const temp = curr;
+            curr = Math.max(prev + nums[i], curr)
+            prev = temp;
+        }
 
-    // 첫 번째 집 포함되고 마지막 집 포함되지 않은 케이스
-    for (let i = 2; i < nums.length - 1; i++) {
-        const current = Math.max(firstHouseDpPrev1, firstHouseDpPrev2 + nums[i]);
-
-        firstHouseDpPrev2 = firstHouseDpPrev1
-        firstHouseDpPrev1 = current
+        return curr
     }
 
-    // 첫 번째 집 포함되지 않고 마지막 집 포함된 케이스
-    for (let i = 3; i < nums.length; i++) {
-        const current = Math.max(lastHouseDpPrev1, lastHouseDpPrev2 + nums[i])
-
-        lastHouseDpPrev2 = lastHouseDpPrev1
-        lastHouseDpPrev1 = current
-    }
-
-    return Math.max(firstHouseDpPrev1, lastHouseDpPrev1)
+    return Math.max(dp(0, nums.length - 1), dp(1, nums.length))
 };
