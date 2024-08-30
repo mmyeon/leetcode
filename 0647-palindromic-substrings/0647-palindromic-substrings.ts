73935180610782cc70f13872752c597ddc2e830f
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-08-30 15:00:24 +0900

    Time: 70 ms (72.49%), Space: 52.4 MB (71.62%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.ts
+++ 0647-palindromic-substrings/0647-palindromic-substrings.ts
@@ -1,12 +1,11 @@
 function countSubstrings(s: string): number {
-
-    let palindromes = [];
+    let palindromeCount = 0;
 
     for (let i = 0; i < s.length; i++) {
         // 홀수 중심의 palindrome
         let start = i, end = i;
         while (0 <= start && end < s.length && s[start] === s[end]) {
-            palindromes.push(s.slice(start, end + 1))
+            palindromeCount++;
             start--;
             end++;
         }
@@ -14,11 +13,11 @@ function countSubstrings(s: string): number {
         // 짝수 중심의 palindrome            
         start = i; end = i + 1;
         while (0 <= start && end < s.length && s[start] === s[end]) {
-            palindromes.push(s.slice(start, end + 1))
+            palindromeCount++;
             start--
             end++
         }
     }
 
-    return palindromes.length
+    return palindromeCount
 };
