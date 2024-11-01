b409b911e97270e6f68367daeb78d074034b00df
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-11-01 12:43:17 +0900

    Time: 123 ms (85.23%), Space: 66.5 MB (42.91%) - LeetHub

---
+++ 2860-sort-by/2860-sort-by.ts
@@ -0,0 +1,5 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (value: JSONValue) => number
+
+function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
+    return arr.sort((a, b) => { return fn(a) - fn(b) });};
