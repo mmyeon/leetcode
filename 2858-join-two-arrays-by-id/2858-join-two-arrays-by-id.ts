36d2eb794769738fdcbda31a67f4344b4fb9e5cd
mmyeon <57064447+mmyeon@users.noreply.github.com>
2024-11-15 11:45:28 +0900

    Time: 298 ms (90.04%), Space: 94.4 MB (57.71%) - LeetHub

--- 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.ts
+++ 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.ts
@@ -2,14 +2,14 @@ type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string
 type ArrayType = { "id": number } & Record<string, JSONValue>;
 
 function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
-    let obj = {};
-    
+    let obj: Record<string, ArrayType> = {};
+
     for (const item of arr1) {
         obj[item["id"]] = item
     }
 
     for (const item of arr2) {
-        obj[item["id"]] ={... obj[item["id"]], ...item}
+        obj[item["id"]] = obj[item["id"]] ? { ...obj[item["id"]], ...item } : item
     }
 
     return Object.values(obj)
